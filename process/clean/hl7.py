04e9d4cda79676e2396f41dfe9bb755329e36ef6
Theo <theo.leffyr@gmail.com>
2018-10-26 14:16:37 +0200

    Add support of code options in the hl7 parser

--- process/clean/hl7.py
+++ process/clean/hl7.py
@@ -5,13 +5,15 @@ def clean_json(filename, output_file=None):
     input_file = open(filename)
     input_file.readline()
 
-    # Remove comments
+    # Remove and store comments
     new_lines = ['{']
+    comments = [None]
     for line in input_file:
         l = line.split("//", 1)
         # remove empty lines, and remove \n
         if l[0] not in '         ':
             new_lines.append(l[0].replace("\n", ""))
+            comments.append(l[1] if len(l) >= 2 else None)
 
     # Handle [{ }] {} from FHIR convention
     lines = new_lines
@@ -32,9 +34,12 @@ def clean_json(filename, output_file=None):
         # One line case { } exception with a \n in it
         m = re.match('''(\s*"[^"]*"\s*:\s*{\s*[^"}\s]+)\s*$''', line)
         if m is not None:
+            # concat in a single line, by extending i+1 and not append to new_line
             next_line = lines[i + 1]
             m_next = re.match('''\s*(\w.*)$''', next_line)
             lines[i + 1] = m.group(1) + m_next.group(1)
+            # also romove the line in comments
+            del comments[i]
             continue
 
         # Multi line case [{ \n ... \n }]
@@ -45,20 +50,23 @@ def clean_json(filename, output_file=None):
         else:
             new_lines.append(line)
 
-    # Handle < >
+    # Handle < > type extraction and codes handling
     lines = new_lines
     new_lines = []
-    for line in lines:
-        # Without "" (ex <boolean>)
-        m = re.match('''(\s*)"([^"]*)"\s*:\s*<([^>]*)>(,*)''', line)
-        if m is not None:
-            new_lines.append('{}"{}<{}>": null{}'.format(m.group(1), m.group(2), m.group(3), m.group(4)))
-            continue
+    for i, line in enumerate(lines):
+        match = re.match('''(\s*)"([^"]*)"\s*:\s*"?<([^>]*)>"?(,*)''', line)
+        if match is not None:
+            given_type = match.group(3)
+            if given_type == 'code':  # We need to get the code options given in comments
+                comment = comments[i]
+                code_match = re.match('''[^|]*(\s[A-Za-z\-]+\s(?:\|\s[A-Za-z\-]+\s)+)[^|]*''', comment)
+                if code_match is not None:
+                    codes = code_match.group(1).strip().split(' | ')
+                    given_type += '=' + '|'.join(codes)
+                else:
+                    raise TypeError('No code provided', match)
 
-        # With "" (ex "<date>")
-        m = re.match('''(\s*)"([^"]*)"\s*:\s*"<([^>]*)>"(,*)''', line)
-        if m is not None:
-            new_lines.append('{}"{}<{}>": null{}'.format(m.group(1), m.group(2), m.group(3), m.group(4)))
+            new_lines.append('{}"{}<{}>": null{}'.format(match.group(1), match.group(2), given_type, match.group(4)))
             continue
         else:
             new_lines.append(line)

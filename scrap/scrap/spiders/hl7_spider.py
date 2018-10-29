d7af625c6a6761ef99cb021e2eba0f462b03a850
Andrea PIERRÉ <kir0ul@users.noreply.github.com>
2018-10-30 00:58:21 +0100

    Save JSON files (#2)

--- scrap/scrap/spiders/hl7_spider.py
+++ scrap/scrap/spiders/hl7_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 import urllib.parse
 import html2text
+import os
 
 
 class Hl7Spider(scrapy.Spider):
@@ -25,15 +26,21 @@ class Hl7Spider(scrapy.Spider):
         links = response.css("tr.frm-contents").css("a").xpath(
             "@href").extract()
         filtered_links = list(filter(lambda x: "#" not in x, links))
+
         for link in filtered_links:
             url = urllib.parse.urljoin(self.root_url, link)
-            self.log('Processing URL: {}'.format(url))
             yield scrapy.Request(url=url, callback=self.parse_links)
 
     def parse_links(self, response):
+        self.log('Processing URL: {}'.format(response.url))
         json_html = response.css("#json").css("#json-inner").css(
-            "pre").extract_first()
+            "pre").extract_first().strip()
         json_text = html2text.html2text(json_html)
-        self.log(json_text)
-        # ToDo: Export JSON to text file
-        pass
+        # self.log(json_text)
+
+        page = response.url.split("/")[-1].replace(".html", "")
+        filename = '{}.json'.format(page)
+        filepath = os.path.join("../json", filename)
+        with open(filepath, 'w') as f:
+            f.write(json_text)
+        self.log('Saved file \"{}\"'.format(filename))

f0d2e1bb70bfa1fffe7df77c6d86b63ffb151eb8
alexis <alexisthual@gmail.com>
2018-11-12 19:16:16 +0100

    Merge resource_list files and add test

--- generate_file_list.py
+++ generate_file_list.py
@@ -2,20 +2,39 @@ import glob
 import json
 import os
 
+from itertools import combinations
+
 EXTENSIONS = ['json', 'yml']
 RESOURCES = './resources'
 
 
+lists = []
+
+# List all available resources for all possible extensions
 for extension in EXTENSIONS:
-    with open(os.path.join(RESOURCES, extension, 'resource_list.json'), 'w+') as f:
-        resultDict = {}
+    list = []
+    for filepath in glob.iglob('{}/{}/*/*/*.{}'.format(RESOURCES, extension, extension), recursive=True):
+        filename = os.path.splitext(os.path.basename(filepath))[0]
+        list.append(filename)
 
-        for filepath in glob.iglob('{}/{}/*/*/*.{}'.format(RESOURCES, extension, extension), recursive=True):
-            path, filename = os.path.split(filepath)
-            filename = os.path.splitext(filename)[0]
+    lists.append(list)
+    print('Listed {} {} fhir resources appart from datatypes.'.format(len(list), extension))
 
-            resultDict[filename] = None
+# Assumes all lists are equal
+for a, b in combinations(range(len(lists)), 2):
+    for x in lists[a]:
+        if x not in lists[b]:
+            raise('error')
+    for x in lists[b]:
+        if x not in lists[a]:
+            raise('error')
 
-        print('Listed {} {} fhir resources appart from datatypes.'.format(len(resultDict), extension))
+print('Comparison test passed.')
 
-        f.write(json.dumps(resultDict))
+# Outputs result dictionary to json file
+resultDict = {}
+for x in lists[0]:
+    resultDict[x] = None
+
+with open(os.path.join(RESOURCES, 'resource_list.json'), 'w+') as f:
+    f.write(json.dumps(resultDict))

d449a73dd60781c72855f3fab232a695c8d01106
Andrea PIERRÉ <kir0ul@users.noreply.github.com>
2018-11-12 01:16:13 +0100

    Forgot some comments

--- process/process.py
+++ process/process.py
@@ -7,13 +7,13 @@ from scrap.scrap.spiders.hl7_spider import Hl7Spider
 from clean.hl7 import Clean
 from convert import yml
 
-# # Scrap the JSON text from hl7 reference
-# process = CrawlerProcess({
-#     "USER_AGENT":
-#     "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
-# })
-# process.crawl(Hl7Spider)
-# process.start()
+# Scrap the JSON text from hl7 reference
+process = CrawlerProcess({
+    "USER_AGENT":
+    "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
+})
+process.crawl(Hl7Spider)
+process.start()
 
 # Get all file to process
 FILE_PATH = os.path.dirname(__file__)

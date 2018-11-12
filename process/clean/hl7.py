63032d4df69522949f046ca5efe9133027fca8e1
Theo <theo.leffyr@gmail.com>
2018-11-12 13:07:42 +0100

    Improve the cleaner to handle all resources

--- process/clean/hl7.py
+++ process/clean/hl7.py
@@ -1,21 +1,22 @@
 import re
+import os
 
-
-class Clean():
+class Clean:
+    @staticmethod
     def clean_json(filename, output_file=None):
         with open(filename) as input_file:
 
             input_file.readline()
 
             # Remove and store comments
-            new_lines = ['{']
-            comments = [None]
+            new_lines = []
+            comments = []
             for line in input_file:
                 l = line.split("//", 1)
                 # remove empty lines, and remove \n
                 if l[0] not in '         ':
                     new_lines.append(l[0].replace("\n", ""))
-                    comments.append(l[1] if len(l) >= 2 else None)
+                    comments.append(l[1] if len(l) >= 2 else '')
 
             # Handle [{ }] {} from FHIR convention
             lines = new_lines
@@ -38,8 +39,8 @@ class Clean():
                         m.group(1), m.group(2), m.group(3), m.group(4)))
                     continue
 
-                # One line case { } exception with a \n in it
-                m = re.match('''(\s*"[^"]*"\s*:\s*{\s*[^"}\s]+)\s*$''', line)
+                # One line case { } / [{ }] exception with a \n in it
+                m = re.match('''(\s*"[^"]*"\s*:\s*\[?{\s*[^"}\]\s]+)\s*$''', line)
                 if m is not None:
                     # concat in a single line, by extending i+1 and not append to new_line
                     next_line = lines[i + 1]
@@ -82,8 +83,9 @@ class Clean():
                         if code_match is not None:
                             codes = code_match.group(1).strip().split(' | ')
                             given_type += '=' + '|'.join(codes)
-                        else:
-                            raise TypeError('No code provided', match)
+                        # Sometimes there is no code options
+                        # else:
+                        #    raise TypeError('No code provided', match)
 
                     new_lines.append('{}"{}<{}{}>": null{}'.format(
                         match.group(1), match.group(2), list_marker,
@@ -92,7 +94,38 @@ class Clean():
                 else:
                     new_lines.append(line)
 
+            # Rm bug lines
+            lines = new_lines
+            new_lines = []
+            kill_me_lines = [
+                'see information codes'
+            ]
+            for i, line in enumerate(lines):
+                if line not in kill_me_lines:
+                    new_lines.append(line)
+
+            # Fix ',' errors at end of line
+            lines = new_lines
+            new_lines = []
+            indents = []
+            for i, line in enumerate(lines):
+                match = re.match('''(\s*).*''', line)
+                indent = len(match[1])
+                last_indent = indents[-1] if len(indents) > 0 else 0
+                if last_indent <= indent:
+                    if not ',' in line:
+                        line += ','
+                if last_indent != indent:
+                    last_line = new_lines[i - 1]
+                    if ',' in last_line:
+                        new_lines[i - 1] = last_line.replace(',', '')
+
+                new_lines.append(line)
+                indents.append(indent)
+
             if output_file is not None:
+                if not os.path.exists(os.path.dirname(output_file)):
+                    os.makedirs(os.path.dirname(output_file))
                 with open(output_file, 'w') as output_file:
                     output_file.write('\n'.join(new_lines))
             else:

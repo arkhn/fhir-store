d4cbb176825e8790c8043ac4876ca2c0a432ca96
Andrea PIERRÉ <kir0ul@users.noreply.github.com>
2018-11-12 01:12:05 +0100

    WIP: Started clean + convert pipeline

--- process/clean/hl7.py
+++ process/clean/hl7.py
@@ -1,85 +1,87 @@
 import re
 
 
-def clean_json(filename, output_file=None):
-    with open(filename) as input_file:
+class Clean():
 
-        input_file.readline()
+   def clean_json(filename, output_file=None):
+        with open(filename) as input_file:
 
-        # Remove and store comments
-        new_lines = ['{']
-        comments = [None]
-        for line in input_file:
-            l = line.split("//", 1)
-            # remove empty lines, and remove \n
-            if l[0] not in '         ':
-                new_lines.append(l[0].replace("\n", ""))
-                comments.append(l[1] if len(l) >= 2 else None)
+            input_file.readline()
 
-        # Handle [{ }] {} from FHIR convention
-        lines = new_lines
-        new_lines = []
-        for i, line in enumerate(lines):
-            # One line case [{ }]
-            m = re.match('''(\s*)\"([^"]*)\"\s*\:\s*\[\{\s*([^}]*)\s+\}\](,*)''', line)
-            if m is not None:
-                new_lines.append('{}"{}<list::{}>": null{}'.format(m.group(1), m.group(2), m.group(3), m.group(4)))
-                continue
+            # Remove and store comments
+            new_lines = ['{']
+            comments = [None]
+            for line in input_file:
+                l = line.split("//", 1)
+                # remove empty lines, and remove \n
+                if l[0] not in '         ':
+                    new_lines.append(l[0].replace("\n", ""))
+                    comments.append(l[1] if len(l) >= 2 else None)
 
-            # One line case { }
-            m = re.match('''(\s*)"([^"]*)"\s*:\s*{\s*([^}]*)\s+}(,*)''', line)
-            if m is not None:
-                new_lines.append('{}"{}<{}>": null{}'.format(m.group(1), m.group(2), m.group(3), m.group(4)))
-                continue
+            # Handle [{ }] {} from FHIR convention
+            lines = new_lines
+            new_lines = []
+            for i, line in enumerate(lines):
+                # One line case [{ }]
+                m = re.match('''(\s*)\"([^"]*)\"\s*\:\s*\[\{\s*([^}]*)\s+\}\](,*)''', line)
+                if m is not None:
+                    new_lines.append('{}"{}<list::{}>": null{}'.format(m.group(1), m.group(2), m.group(3), m.group(4)))
+                    continue
 
-            # One line case { } exception with a \n in it
-            m = re.match('''(\s*"[^"]*"\s*:\s*{\s*[^"}\s]+)\s*$''', line)
-            if m is not None:
-                # concat in a single line, by extending i+1 and not append to new_line
-                next_line = lines[i + 1]
-                m_next = re.match('''\s*(\w.*)$''', next_line)
-                lines[i + 1] = m.group(1) + m_next.group(1)
-                # also romove the line in comments
-                del comments[i]
-                continue
+                # One line case { }
+                m = re.match('''(\s*)"([^"]*)"\s*:\s*{\s*([^}]*)\s+}(,*)''', line)
+                if m is not None:
+                    new_lines.append('{}"{}<{}>": null{}'.format(m.group(1), m.group(2), m.group(3), m.group(4)))
+                    continue
 
-            # Multi line case [{ \n ... \n }]
-            m = re.match('''(\s*)\"([^"]*)\"\s*\:\s*\[\{\s*''', line)
-            if m is not None:
-                new_lines.append('{}"{}<list>": {}'.format(m.group(1), m.group(2), '[{'))
-                continue
-            else:
-                new_lines.append(line)
+                # One line case { } exception with a \n in it
+                m = re.match('''(\s*"[^"]*"\s*:\s*{\s*[^"}\s]+)\s*$''', line)
+                if m is not None:
+                    # concat in a single line, by extending i+1 and not append to new_line
+                    next_line = lines[i + 1]
+                    m_next = re.match('''\s*(\w.*)$''', next_line)
+                    lines[i + 1] = m.group(1) + m_next.group(1)
+                    # also romove the line in comments
+                    del comments[i]
+                    continue
 
-        # Handle < > type extraction and codes handling
-        lines = new_lines
-        new_lines = []
-        for i, line in enumerate(lines):
-            match = re.match('''(\s*)"([^"]*)"\s*:\s*\[?"?<([^>]*)>"?\]?(,*)''', line)
-            if match is not None:
-                given_type = match.group(3)
-                # Test if [ ] present
-                if re.match('''(\s*)"([^"]*)"\s*:\s*\["?<([^>]*)>"?\](,*)''', line):
-                    list_marker = 'list::'
+                # Multi line case [{ \n ... \n }]
+                m = re.match('''(\s*)\"([^"]*)\"\s*\:\s*\[\{\s*''', line)
+                if m is not None:
+                    new_lines.append('{}"{}<list>": {}'.format(m.group(1), m.group(2), '[{'))
+                    continue
                 else:
-                    list_marker = ''
+                    new_lines.append(line)
 
-                if given_type == 'code':  # We need to get the code options given in comments
-                    comment = comments[i]
-                    code_match = re.match('''[^|]*(\s[A-Za-z\-]+\s(?:\|\s[A-Za-z\-]+\s)+)[^|]*''', comment)
-                    if code_match is not None:
-                        codes = code_match.group(1).strip().split(' | ')
-                        given_type += '=' + '|'.join(codes)
+            # Handle < > type extraction and codes handling
+            lines = new_lines
+            new_lines = []
+            for i, line in enumerate(lines):
+                match = re.match('''(\s*)"([^"]*)"\s*:\s*\[?"?<([^>]*)>"?\]?(,*)''', line)
+                if match is not None:
+                    given_type = match.group(3)
+                    # Test if [ ] present
+                    if re.match('''(\s*)"([^"]*)"\s*:\s*\["?<([^>]*)>"?\](,*)''', line):
+                        list_marker = 'list::'
                     else:
-                        raise TypeError('No code provided', match)
+                        list_marker = ''
 
-                new_lines.append('{}"{}<{}{}>": null{}'.format(match.group(1), match.group(2), list_marker, given_type, match.group(4)))
-                continue
+                    if given_type == 'code':  # We need to get the code options given in comments
+                        comment = comments[i]
+                        code_match = re.match('''[^|]*(\s[A-Za-z\-]+\s(?:\|\s[A-Za-z\-]+\s)+)[^|]*''', comment)
+                        if code_match is not None:
+                            codes = code_match.group(1).strip().split(' | ')
+                            given_type += '=' + '|'.join(codes)
+                        else:
+                            raise TypeError('No code provided', match)
+
+                    new_lines.append('{}"{}<{}{}>": null{}'.format(match.group(1), match.group(2), list_marker, given_type, match.group(4)))
+                    continue
+                else:
+                    new_lines.append(line)
+
+            if output_file is not None:
+                with open(output_file, 'w') as output_file:
+                    output_file.write('\n'.join(new_lines))
             else:
-                new_lines.append(line)
-
-        if output_file is not None:
-            with open(output_file, 'w') as output_file:
-                output_file.write('\n'.join(new_lines))
-        else:
-            return '\n'.join(new_lines)
+                return '\n'.join(new_lines)
--- process/process.py
+++ process/process.py
@@ -1,12 +1,40 @@
 # -*- coding: utf-8 -*-
 
+import os
 from scrapy.crawler import CrawlerProcess
-from scrap.scrap.spiders.hl7_spider import Hl7Spider
 
-# Scrap the JSON text from hl7 reference
-process = CrawlerProcess({
-    "USER_AGENT":
-    "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
-})
-process.crawl(Hl7Spider)
-process.start()
+from scrap.scrap.spiders.hl7_spider import Hl7Spider
+from clean.hl7 import Clean
+from convert import yml
+
+
+# # Scrap the JSON text from hl7 reference
+# process = CrawlerProcess({
+#     "USER_AGENT":
+#     "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
+# })
+# process.crawl(Hl7Spider)
+# process.start()
+
+# Get all file to process
+FILE_PATH = os.path.dirname(__file__)
+ROOT_FOLDER_SRC_NAME = "json"
+ROOT_FOLDER_DEST_NAME = "yaml"
+ROOT_FOLDER_SRC_PATH = os.path.join(FILE_PATH, os.pardir, ROOT_FOLDER_SRC_NAME)
+ROOT_FOLDER_DEST_PATH = os.path.join(FILE_PATH, os.pardir, ROOT_FOLDER_DEST_NAME)
+
+for root, dirs, files in os.walk(ROOT_FOLDER_SRC_PATH):
+    for fname in files:
+
+        # Clean JSON file
+        json_file_path = os.path.join(root, fname)
+        yaml_file_path = json_file_path.replace("json", "yaml")
+        Clean().clean_json(json_file_path, json_file_path)
+
+        # Convert to YAML
+        with open(json_file_path) as input_file:
+            input_file.readline()
+            new_yaml_file = json_to_yml(input_file)
+            with open(yaml_file_path, 'w') as output_file:
+                output_file.write(new_yaml_file)
+
